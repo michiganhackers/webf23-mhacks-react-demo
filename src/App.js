91386107cbf89d1c561eb918076b91dc541c3943
Brian Travis <bztravis88@gmail.com>
2023-11-18 00:31:12 -0500

    Add finished planning code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const defaultTasks = [
+  {
+    id: crypto.randomUUID(),
+    title: 'Take out the trash',
+    completed: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Do laundry',
+    completed: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Go to MHacks',
+    completed: true,
+  },
+]
 
 function App() {
+  const getTasksFromStorage = () => {
+    const storedTasks =
+      JSON.parse(localStorage.getItem('tasks')) || defaultTasks
+    return storedTasks
+  }
+
+  const [tasks, setTasks] = useState(getTasksFromStorage)
+  const [taskInput, setTaskInput] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks])
+
+  const handleAddTask = () => {
+    if (!taskInput) return
+
+    const newTask = {
+      id: crypto.randomUUID(),
+      title: taskInput,
+      completed: false,
+    }
+
+    setTasks([newTask, ...tasks])
+    setTaskInput('')
+  }
+
+  const toggleCompleted = (id, checked) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) return { ...task, completed: checked }
+        return task
+      })
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+      />
+      <button className="addButton" onClick={() => handleAddTask()}>
+        +
+      </button>
+      {tasks.map((task) => {
+        return (
+          <div className="todo">
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onClick={() => {
+                toggleCompleted(task.id, !task.completed)
+              }}
+            />
+            <p>{task.title}</p>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
